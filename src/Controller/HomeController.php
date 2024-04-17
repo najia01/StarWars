ba8910d60f91e87843d4c0a6ae1e80d65eefb4c5
najia01 <validress@outlook.fr>
2024-04-17 21:56:30 +0200

    ajout planete et pagination page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Service\StarWarsApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class HomeController extends AbstractController
@@ -16,14 +17,17 @@ class HomeController extends AbstractController
         
     }
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        
+        $page = $request->query->getInt('page', 1); // Get 'page' parameter from the query, default is 1
         return $this->render('home/index.html.twig', [
-            'personnages' => $this->starWarsApiService->getPersonnages(),
+            'personnages' => $this->starWarsApiService->getPersonnages($page),
+            'currentPage' => $page,
+            'nextPage' => $page + 1,
+            'prevPage' => $page > 1 ? $page - 1 : null
         ]);
     }
-
+    
 
     #[Route('/personnage/{id}', name: 'app_personnage', requirements: ['id' => '\d+'])]
     public function personnage(int $id): Response
@@ -35,4 +39,20 @@ class HomeController extends AbstractController
      'personnage' =>$this->starWarsApiService->getPersonnage($id),
      ]);
     }
+
+
+    #[Route('/planets', name: 'app_planets')]
+public function planets(Request $request): Response
+{
+    $page = $request->query->getInt('page', 1);
+    $planetsData = $this->starWarsApiService->getPlanets($page);
+
+    return $this->render('home/planets.html.twig', [
+        'planets' => $planetsData['results'],
+        'currentPage' => $page,
+        'nextPage' => $page + 1,
+        'prevPage' => $page > 1 ? $page - 1 : null
+    ]);
+}
+
 }
--- src/Service/StarWarsApiService.php
+++ src/Service/StarWarsApiService.php
@@ -8,46 +8,46 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class StarWarsApiService
 {
-    private const BASE_URL = 'https://swapi.py4e.com/api/people/';
+    private const BASE_URL = 'https://swapi.py4e.com/api/';
     private const ITEM = 'ITEM';
     private const COLLECTION = 'COLLECTION';
-    
 
     public function __construct(
         private readonly HttpClientInterface $httpClient
     )
     {
-
     }
 
-
-    public function getPersonnages() : array {
-
-        return $this->makeRequest(self::COLLECTION);
-
-
+    public function getPersonnages(int $page = 1) : array {
+        return $this->makeRequest('people', null, $page);
     }
-
-
-    public function getPersonnage(int $id) : array{
-
-        return $this->makeRequest(self::ITEM, $id);
+    
+    public function getPersonnage(int $id) : array {
+        return $this->makeRequest('people', $id);
     }
-
-
-    private function makeRequest(string  $type, ?int $id = null): array {
-        $url = $id ? self::BASE_URL.$id : self::BASE_URL;
-
-        $response =  $this->httpClient->request('GET', $url);
-
-        $data =  match($type){
-            self::COLLECTION => $response->toArray()['results'],
-            self::ITEM => $response ->toArray(),
-        };
-
-        return $data;
-
-
+    
+    // Ajout d'une méthode pour récupérer les planètes
+    public function getPlanets(int $page = 1) : array {
+        return $this->makeRequest('planets', null, $page);
     }
+    
 
+    private function makeRequest(string $resourceType, ?int $id = null, ?int $page = null): array {
+        // Construire l'URL en fonction du type de ressource et de l'ID éventuel
+        $url = $id ? self::BASE_URL . $resourceType . '/' . $id : self::BASE_URL . $resourceType;
+    
+        // Préparation des paramètres de requête
+        $params = [];
+        if ($page) {
+            $params['query'] = ['page' => $page];
+        }
+    
+        // Effectuer la requête HTTP
+        $response = $this->httpClient->request('GET', $url, $params);
+    
+        // Retourner la réponse sous forme de tableau
+        return $response->toArray();
+    }
+    
+    
 }
